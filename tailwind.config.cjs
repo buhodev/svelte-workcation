91a867c4ec537c3f04fdb9b7a5388e583165864d
Carlos Ivanchuk <buhodev@proton.me>
2022-01-09 20:15:45 -0400

    copy files content from the repository

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '80': '20rem',
+      },
+      padding: {
+        '5/6': '83.3333333%'
+      }
+    },
   },
 
   plugins: [require('@tailwindcss/forms')],
